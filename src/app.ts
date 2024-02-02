0c2229ccdfff6258ed9dd325771ab087f6530369
EmptyNode <96117153+EmptyNode@users.noreply.github.com>
2024-02-02 23:58:31 +0530

    payment route updated, all type of stats api created like bar chart, new routes at app.ts are added

--- src/app.ts
+++ src/app.ts
@@ -40,6 +40,8 @@ app.get("/", (req, res) => {
 // Using Routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/product", productRoute);
+app.use("/api/v1/order", orderRoute);
+app.use("/api/v1/payment", paymentRoute);
 app.use("/api/v1/dashboard", dashboardRoute);
 
 app.use("/uploads", express.static("uploads"));
